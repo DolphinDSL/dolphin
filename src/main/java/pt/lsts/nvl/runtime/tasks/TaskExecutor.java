297787538f77d941e2353510300d9e3bb92a96ce
Eduardo Marques <eduardorbmarques@gmail.com>
2017-04-19 16:54:15 +0100

    Task model - work in progress

--- src/main/java/pt/lsts/nvl/runtime/tasks/TaskExecutor.java
+++ src/main/java/pt/lsts/nvl/runtime/tasks/TaskExecutor.java
@@ -6,15 +6,14 @@ import java.util.List;
 import java.util.Optional;
 
 import pt.lsts.nvl.runtime.NVLRuntime;
+import pt.lsts.nvl.runtime.NVLRuntimeException;
 import pt.lsts.nvl.runtime.NVLVehicle;
 import pt.lsts.nvl.runtime.VehicleRequirements;
 import pt.lsts.nvl.util.Clock;
 
 public abstract class TaskExecutor<T extends Task> {
-
   /**
    * Task executor state.
-   *
    */
   public enum State {
     /** INITIALIZING. */
@@ -26,37 +25,38 @@ public abstract class TaskExecutor<T extends Task> {
     /** Completed */
     COMPLETED;
   }
-  
+
   private final T task;
   private State state;
   private double startTime;
   private double timeElapsed;
   private List<NVLVehicle> boundVehicles;
-  
+
   TaskExecutor(T theTask) {
     task = theTask;
     state = State.INITIALIZING;
     startTime = -1;
     boundVehicles = Collections.emptyList();
   }
-  
+
   public final T getTask() { 
     return task;
   }
-  
+
   public final State getState() {
     return state;
   }
-  
+
   public final double startTime() {
     return startTime;
   }
-  
+
   public final List<NVLVehicle> getVehicles() {
     return boundVehicles; 
   }
-  
+
   public boolean initialize(NVLRuntime runtime) {
+    requireState(State.INITIALIZING);
     onInitialize();
     List<NVLVehicle> reservedVehicles = new ArrayList<>();
     List<NVLVehicle> allVehicles = runtime.getVehicles(x -> true);
@@ -71,25 +71,32 @@ public abstract class TaskExecutor<T extends Task> {
     state = State.READY;
     return true;    
   }
-  
+
   public final void start() {
+    requireState(State.READY);
     onStart();
     startTime = Clock.now();
     state = State.EXECUTING;
   }
 
   public final void step() {
+    requireState(State.EXECUTING);
     timeElapsed = Clock.now() - startTime;
     onStep();
   }
-  
+
   public final double clock() {
     return timeElapsed;
   }
-  
+
   protected abstract void onInitialize();
   protected abstract void onStart();
   protected abstract void onStep();
   protected abstract void onCompletion();
 
+  private void requireState(State s) {
+    if (getState() != s) {
+      throw new NVLRuntimeException("Expected " + s + " state");
+    }
+  }
 }
