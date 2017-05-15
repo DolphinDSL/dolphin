79fb5cea96b5fe4fecc4c224b0eef46fdf3148dc
Eduardo Marques <eduardorbmarques@gmail.com>
2017-05-15 17:18:15 +0100

    Work in progress

--- nvl/src/main/java/pt/lsts/nvl/runtime/NVLPlatform.java
+++ nvl/src/main/java/pt/lsts/nvl/runtime/NVLPlatform.java
@@ -7,19 +7,8 @@ import pt.lsts.nvl.util.Debuggable;
 
 public interface NVLPlatform extends Debuggable {
   
-  String NVL_PLATFORM_PROPERTY = "nvl.platform.class";
-  
-  static NVLPlatform create() {
-    try {
-      Class<?> theClass = Class.forName(System.getProperty(NVL_PLATFORM_PROPERTY));
-      return (NVLPlatform) theClass.newInstance();
-    }
-    catch(ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-      throw new NVLExecutionException(e); 
-    }
-  }
-  
   List<NVLVehicle> getConnectedVehicles();
   
   PlatformTask getPlatformTask(String id);
+  
 }
