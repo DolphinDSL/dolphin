5ae5bbc453d035c2d23fef1dc51725eb1c6dc72e
Eduardo Marques <eduardorbmarques@gmail.com>
2017-05-26 16:08:57 +0100

    id now a regular expression

--- nvl-core/src/main/java/pt/lsts/nvl/runtime/VehicleRequirements.java
+++ nvl-core/src/main/java/pt/lsts/nvl/runtime/VehicleRequirements.java
@@ -10,24 +10,24 @@ public final class VehicleRequirements {
   private List<PayloadComponent> requiredPayload;
   private Position areaCenter;
   private double areaRadius;
-  
+
   public VehicleRequirements() {
-      requiredId = null;
-      requiredPayload = null;
-      areaCenter = null;
-      areaRadius = 0;  
+    requiredId = null;
+    requiredPayload = null;
+    areaCenter = null;
+    areaRadius = 0;  
   }
 
 
   public VehicleRequirements type(String type) {
     requiredType = type;
-    return this; // for chained: http://blog.crisp.se/2013/10/09/perlundholm/another-builder-pattern-for-java
+    return this; 
   }
-  
+
   public VehicleRequirements name(String name) {
-      requiredId = name;
-      return this; 
-    }
+    requiredId = name;
+    return this; 
+  }
 
   public VehicleRequirements payload(List<PayloadComponent> components) {
     requiredPayload = components;
@@ -48,17 +48,17 @@ public final class VehicleRequirements {
   }
 
   public boolean matchedBy(NVLVehicle v) {
-   return     (requiredId == null || requiredId.equals(v.getId()))
-           && (requiredType == null || v.getType().equals(requiredType))
-           && ((requiredPayload == null || v.getPayload().containsAll(requiredPayload)))
-           && ((areaCenter == null || v.getPosition().near(areaCenter, areaRadius) ));
+    return (requiredId == null || v.getId().matches(requiredId))
+        && (requiredType == null || v.getType().equals(requiredType))
+        && ((requiredPayload == null || v.getPayload().containsAll(requiredPayload)))
+        && ((areaCenter == null || v.getPosition().near(areaCenter, areaRadius) ));
   }
 
   /**
    * @return the requiredType
    */
   public String getRequiredType() {
-  	return requiredType;
+    return requiredType;
   }
 
   /**
@@ -67,69 +67,65 @@ public final class VehicleRequirements {
   public void setRequiredId(String requiredId) {
     this.requiredId = requiredId;
   }
-  
-  
+
+
   /**
    * @param requiredType the requiredType to set
    */
   public void setRequiredType(String requiredType) {
-  	this.requiredType = requiredType;
+    this.requiredType = requiredType;
   }
 
   /**
    * @return the requiredPayload
    */
   public List<PayloadComponent> getRequiredPayload() {
-  	return requiredPayload;
+    return requiredPayload;
   }
 
   /**
    * @param requiredPayload the requiredPayload to set
    */
   public void setRequiredPayload(List<PayloadComponent> requiredPayload) {
-  	this.requiredPayload = requiredPayload;
+    this.requiredPayload = requiredPayload;
   }
 
   /**
    * @return the areaCenter
    */
   public Position getAreaCenter() {
-  	return areaCenter;
+    return areaCenter;
   }
 
   /**
    * @param areaCenter the areaCenter to set
    */
   public void setAreaCenter(Position areaCenter) {
-  	this.areaCenter = areaCenter;
+    this.areaCenter = areaCenter;
   }
 
   /**
    * @return the areaRadius
    */
   public double getAreaRadius() {
-  	return areaRadius;
+    return areaRadius;
   }
 
   /**
    * @param areaRadius the areaRadius to set
    */
   public void setAreaRadius(double areaRadius) {
-  	this.areaRadius = areaRadius;
+    this.areaRadius = areaRadius;
   }
-  
+
   @Override
   public String toString() {
     Function<Object, String> str = x -> (x != null ? x.toString() : "<null>");
-    
+
     return String.format("id=%s,type=%s,payload=%s,area=%s",
         str.apply(requiredId), 
         str.apply(requiredType),
         str.apply(requiredPayload),
         str.apply(areaCenter) );
   }
-
-
-
-
 }
