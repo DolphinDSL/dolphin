2f68f767cd48ae35c48f8d4a91a889ed97a49ff1
Eduardo Marques <eduardorbmarques@gmail.com>
2017-06-01 01:53:54 +0100

    wip

--- nvl-imc/src/main/java/pt/lsts/nvl/imc/AbstractIMCPlanExecutor.java
+++ nvl-imc/src/main/java/pt/lsts/nvl/imc/AbstractIMCPlanExecutor.java
@@ -18,12 +18,12 @@ public abstract class AbstractIMCPlanExecutor extends PlatformTaskExecutor {
   private static final double WARMUP_TIME = 2.0;
   private static final double PLAN_CONTROL_STATE_TIMEOUT = 5.0;
   private static final AtomicInteger SEQ_ID_GENERATOR = new AtomicInteger(0);
-  
+
   private Variable<PlanControlState> pcsVar; 
 
   protected AbstractIMCPlanExecutor(AbstractIMCPlanTask theTask) {
     super(theTask);
-    
+
   }
 
   protected abstract void sendMessageToVehicle(IMCMessage msg);
@@ -38,7 +38,7 @@ public abstract class AbstractIMCPlanExecutor extends PlatformTaskExecutor {
   protected final void  onStart() {
     pcsVar = new Variable<>();
     setup(); // platform specific setup
-    
+
     // Prepare plan control message and send it to vehicle
     PlanControl pc = new PlanControl();
     pc.setPlanId(getTask().getId());
@@ -51,63 +51,62 @@ public abstract class AbstractIMCPlanExecutor extends PlatformTaskExecutor {
     pc.setRequestId(SEQ_ID_GENERATOR.incrementAndGet());
     pc.setFlags(PlanControl.FLG_CALIBRATE); 
     sendMessageToVehicle(pc);  
-    
+
     // We're done
     d("Started %s on %s", getTask().getId(), getNode().getId());
   }
-  
+
   protected final void onStateUpdate(PlanControlState pcs) {
     pcsVar.set(pcs, timeElapsed());
   }
 
   @Override
   protected CompletionState onStep() {
-      CompletionState completionState =  new CompletionState(CompletionState.Type.IN_PROGRESS);
+    CompletionState completionState =  new CompletionState(CompletionState.Type.IN_PROGRESS);
+    if (timeElapsed() > WARMUP_TIME) {
       if (! pcsVar.hasFreshValue()) {
-          d("%f", pcsVar.age(timeElapsed()));
-          if (pcsVar.age(timeElapsed()) >= PLAN_CONTROL_STATE_TIMEOUT) {
-              
-              d("PlanControlState timeout!");
-              completionState = new CompletionState(CompletionState.Type.ERROR);
-          }
+        d("%f", pcsVar.age(timeElapsed()));
+        if (pcsVar.age(timeElapsed()) >= PLAN_CONTROL_STATE_TIMEOUT) {
+          d("PlanControlState timeout!");
+          completionState = new CompletionState(CompletionState.Type.ERROR);
+        }
       } else {
-          PlanControlState pcs = pcsVar.get();
-          if (!getTask().getId().equals(pcs.getPlanId())) {
-              if (timeElapsed() > WARMUP_TIME) {
+        PlanControlState pcs = pcsVar.get();
+        if (!getTask().getId().equals(pcs.getPlanId())) {
+          completionState = new CompletionState(CompletionState.Type.ERROR);
+          d("Wrong plan id: %s != %s", pcs.getPlanId(), getTask().getId());
+        } else {
+          switch (pcs.getState()) {
+            case BLOCKED:
+              d("ERROR");
+              completionState = new CompletionState(CompletionState.Type.ERROR);
+              break;
+            case EXECUTING:
+            case INITIALIZING:
+              break;
+            case READY:
+              d("Terminated %s on %s : %s", getTask().getId(), getNode().getId(), pcs.getLastOutcome());
+              switch (pcs.getLastOutcome()) {
+                case FAILURE:
+                case NONE:
                   completionState = new CompletionState(CompletionState.Type.ERROR);
-                  d("Wrong plan id: %s != %s", pcs.getPlanId(), getTask().getId());
+                  d("Failure!");
+                  break;
+                case SUCCESS:
+                  completionState = new CompletionState(CompletionState.Type.DONE);
+                  d("IMC plan completed!");
+                  break;
+                default:
+                  throw new EnvironmentException();
               }
-          } else {
-              switch (pcs.getState()) {
-                  case BLOCKED:
-                      d("ERROR");
-                      completionState = new CompletionState(CompletionState.Type.ERROR);
-                      break;
-                  case EXECUTING:
-                  case INITIALIZING:
-                      break;
-                  case READY:
-                      d("Terminated %s on %s : %s", getTask().getId(), getNode().getId(), pcs.getLastOutcome());
-                      switch (pcs.getLastOutcome()) {
-                          case FAILURE:
-                          case NONE:
-                              completionState = new CompletionState(CompletionState.Type.ERROR);
-                              d("Failure!");
-                              break;
-                          case SUCCESS:
-                              completionState = new CompletionState(CompletionState.Type.DONE);
-                              d("IMC plan completed!");
-                              break;
-                          default:
-                              throw new EnvironmentException();
-                      }
-                      break;
-                  default:
-                      throw new EnvironmentException();
-              }
+              break;
+            default:
+              throw new EnvironmentException();
           }
+        }
       }
-      return completionState;
+    }
+    return completionState;
   }
 
   @Override
