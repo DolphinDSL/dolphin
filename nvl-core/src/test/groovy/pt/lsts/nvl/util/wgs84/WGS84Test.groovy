f9454903fdb74946b5b49b265a1037c115a5cbe0
Eduardo Marques <eduardorbmarques@gmail.com>
2017-06-06 19:27:06 +0100

    Some cleanup

--- nvl-core/src/test/groovy/pt/lsts/nvl/util/wgs84/WGS84Test.groovy
+++ nvl-core/src/test/groovy/pt/lsts/nvl/util/wgs84/WGS84Test.groovy
@@ -9,24 +9,20 @@ import spock.lang.Specification
 
 class WGS84Test extends Specification {
   
-  static class Location {
-    Position wgs84;
-    ECEF ecef;
-  }
   static final Position APDL = Position.fromDegrees(41.185781, -8.70606486, 0.0);
   static final Position APDL_B1 = Position.fromDegrees(41.184742, -8.704601, 0.0);
   static final Position APDL_B2 = Position.fromDegrees(41.186057, -8.706045, 0.0);
   
-  static final double TOL = 1e-06;
-  
+  static final double LLH_TOL = 1e-06;
+  static final double NED_TOL = 1e-01;
 
   def 'To ECEF'() {
     when:
       def actual = WGS84.toECEF(pos)
     then:
-      Math.abs(actual.x - expected.x) <= TOL
-      Math.abs(actual.y - expected.y) <= TOL
-      Math.abs(actual.z - expected.z) <= TOL
+      Math.abs(actual.x - expected.x) <= LLH_TOL
+      Math.abs(actual.y - expected.y) <= LLH_TOL
+      Math.abs(actual.z - expected.z) <= LLH_TOL
       
     where:
       pos << APDL
@@ -37,9 +33,9 @@ class WGS84Test extends Specification {
     when:
       def pos2 = WGS84.toECEF(pos).toWGS84()
     then:
-      Math.abs(pos.lat - pos2.lat) <= TOL
-      Math.abs(pos.lon - pos2.lon) <= TOL
-      Math.abs(pos.hae - pos2.hae) <= TOL
+      Math.abs(pos.lat - pos2.lat) <= LLH_TOL
+      Math.abs(pos.lon - pos2.lon) <= LLH_TOL
+      Math.abs(pos.hae - pos2.hae) <= LLH_TOL
       
     where:
       pos << [APDL, APDL_B1, APDL_B2]
@@ -49,7 +45,7 @@ class WGS84Test extends Specification {
     when:
       def actual = a.distanceTo(b)
     then:
-      Math.abs(actual - expected) < 0.1
+      Math.abs(actual - expected) <= NED_TOL
     where:
       a <<         [APDL,       APDL_B1,    APDL_B2,    APDL]
       b <<         [APDL_B1,    APDL_B2,    APDL,       APDL]
@@ -60,9 +56,9 @@ class WGS84Test extends Specification {
     when:
       NED ned = WGS84.displacement(a,b)
     then:
-      Math.abs(ned.north - n) <= 0.1
-      Math.abs(ned.east - e) <= 0.1
-      Math.abs(ned.down) <= 0.1
+      Math.abs(ned.north - n) <= NED_TOL
+      Math.abs(ned.east - e) <= NED_TOL
+      Math.abs(ned.down) <= NED_TOL
     where:
       a <<         [APDL,       APDL_B1,    APDL_B2,    APDL]
       b <<         [APDL_B1,    APDL_B2,    APDL,       APDL]
@@ -74,8 +70,8 @@ class WGS84Test extends Specification {
     when:
       Position pos2 = WGS84.displace(ref, new NED(n, e, 0))
     then:
-      Math.abs(pos.lat - pos2.lat) <= TOL
-      Math.abs(pos.lon - pos2.lon) <= TOL
+      Math.abs(pos.lat - pos2.lat) <= LLH_TOL
+      Math.abs(pos.lon - pos2.lon) <= LLH_TOL
     where:
       ref <<         [APDL,       APDL_B1,    APDL_B2,    APDL]
       n <<         [-115.3877,  146.04161,  -30.651871,  0]
