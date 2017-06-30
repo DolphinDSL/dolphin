2535f2994d799f4c9db53586ba7091511ceb132b
kmolima <keilalima.cv@gmail.com>
2017-06-30 01:29:29 +0100

    Payload provisory work around

--- nvl-imc/src/main/java/pt/lsts/nvl/imc/AbstractIMCPlanTask.java
+++ nvl-imc/src/main/java/pt/lsts/nvl/imc/AbstractIMCPlanTask.java
@@ -3,11 +3,14 @@ package pt.lsts.nvl.imc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 
 import pt.lsts.imc.IMCMessage;
 import pt.lsts.imc.PlanManeuver;
 import pt.lsts.imc.PlanSpecification;
 import pt.lsts.imc.SetEntityParameters;
+import pt.lsts.imc.EntityParameter;
 import pt.lsts.nvl.runtime.NodeFilter;
 import pt.lsts.nvl.runtime.Payload;
 import pt.lsts.nvl.runtime.PayloadComponent;
@@ -17,6 +20,7 @@ public abstract class AbstractIMCPlanTask extends PlatformTask {
 
   private final PlanSpecification planSpec;
   private final Payload payload;
+  private boolean isActive = false;
   protected AbstractIMCPlanTask(String id) {
     this(id, null);
   }
@@ -39,20 +43,29 @@ public abstract class AbstractIMCPlanTask extends PlatformTask {
     for(PlanManeuver maneuver: ps.getManeuvers()){
       for(IMCMessage action: maneuver.getStartActions()) {
         if ( action instanceof SetEntityParameters) {
-          PayloadComponent payload = new PayloadComponent(((SetEntityParameters) action).getName());
+          SetEntityParameters entityP = ((SetEntityParameters) action);
+          PayloadComponent payload = new PayloadComponent(entityP.getName());
           d("  > maneuver %s: Payload %s ", maneuver.getManeuverId(), payload.getName());
       
-          if (! components.contains(payload)) {
-            components.add(new PayloadComponent(payload.getName()));
-          }
-          // TODO: are parameters always the same?
-          // TODO: meaning of Active=true|false 
-//          Map<String,String> params = new HashMap<>();
-//          for(EntityParameter param:payload.getParams()){
-//            d("    > parameter: %s = %s", param.getName(), param.getValue());
-//            params.put(param.getName(),param.getValue());
-//
+//          if (! components.contains(payload)) {
+//            components.add(new PayloadComponent(payload.getName()));
 //          }
+          // TODO: are parameters always the same?
+          // TODO: meaning of Active=true|false
+          
+          Map<String,String> params = new HashMap<>();
+          for(EntityParameter param:entityP.getParams()){
+//            d("    > parameter: %s = %s", param.getName(), param.getValue());
+        	  if(param.getName().equalsIgnoreCase("Active") && param.getValue().equalsIgnoreCase("true")){
+        		  isActive = true;
+        	  }
+//
+          }
+          
+          if (! components.contains(payload) && isActive) {
+              components.add(new PayloadComponent(payload.getName()));
+            }
+          isActive = false;
         }
       }
       d("Payload components: %s", components);
