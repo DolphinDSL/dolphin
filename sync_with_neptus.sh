f976ac6bb9dc7acf6231e5a6c194bbc76018940d
Eduardo Marques <eduardorbmarques@gmail.com>
2017-07-04 01:06:19 +0100

    sync_with_neptus.sh: path has changed

--- sync_with_neptus.sh
+++ sync_with_neptus.sh
@@ -1,3 +1,3 @@
 mvn clean install
-rm -fr  $1/plugins-dev/nvl_runtime/lib/*
-cp nvl-neptus/target/nvl-neptus-0.1-SNAPSHOT.jar $1/plugins-dev/nvl_runtime/lib/
+rm -fr  $1/plugins-dev/dolphin/lib/*
+cp nvl-neptus/target/nvl-neptus-0.1-SNAPSHOT.jar $1/plugins-dev/dolphin/lib/
