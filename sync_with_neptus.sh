ac5e8dd0451ec5078429dc54054b16f440834904
Eduardo Marques <eduardorbmarques@gmail.com>
2017-07-17 17:49:16 +0100

    Updated JAR file name

--- sync_with_neptus.sh
+++ sync_with_neptus.sh
@@ -1,3 +1,3 @@
 mvn clean install
 rm -fr  $1/plugins-dev/dolphin/lib/*
-cp nvl-neptus/target/nvl-neptus-0.1-SNAPSHOT.jar $1/plugins-dev/dolphin/lib/
+cp nvl-neptus/target/dolphin-neptus-0.1-SNAPSHOT.jar $1/plugins-dev/dolphin/lib/
