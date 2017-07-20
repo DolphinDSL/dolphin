e8b087658d617b5e2f9656559181eba4581a580f
kmolima <keilalima.cv@gmail.com>
2017-07-20 12:55:44 +0100

    In support of #16 rename nvl-neptus -> dolphin-neptus

--- sync_with_neptus.sh
+++ sync_with_neptus.sh
@@ -1,3 +1,3 @@
 mvn clean install
 rm -fr  $1/plugins-dev/dolphin/lib/*
-cp nvl-neptus/target/dolphin-neptus-0.1-SNAPSHOT.jar $1/plugins-dev/dolphin/lib/
+cp dolphin-neptus/target/dolphin-neptus-0.1-SNAPSHOT.jar $1/plugins-dev/dolphin/lib/
