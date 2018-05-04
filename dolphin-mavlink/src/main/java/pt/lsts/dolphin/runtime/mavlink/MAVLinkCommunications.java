a9379ee78d07cb443d82f32951907122586acf5a
Eduardo Marques <eduardorbmarques@gmail.com>
2018-05-04 14:50:09 +0100

    MAVlink support (work in progress) Note: mavlink classes need to be generated for version 1.0 of the protocol only

--- dolphin-mavlink/src/main/java/pt/lsts/dolphin/runtime/mavlink/MAVLinkCommunications.java
+++ dolphin-mavlink/src/main/java/pt/lsts/dolphin/runtime/mavlink/MAVLinkCommunications.java
@@ -91,9 +91,15 @@ public class MAVLinkCommunications extends Thread implements Debuggable {
   public void run() {
     active = true;
     while (active) {
-      double timeNow = Clock.now();
-      handleHeartbeats(timeNow);
-      handleIncomingMessages();
+      try {
+        double timeNow = Clock.now();
+        handleHeartbeats(timeNow);
+        handleIncomingMessages();
+      }
+      catch(RuntimeException e) {
+        d("Unexpected exception: %s", e.toString());
+        e.printStackTrace(System.err);
+      }
     }
   }
   private double lastHB = 0;
@@ -102,6 +108,7 @@ public class MAVLinkCommunications extends Thread implements Debuggable {
     if (timeNow - lastHB >= HEARTBEAT_PERIOD) {
       lastHB = timeNow;
       for (MAVLinkNode node : nodes.values()) {
+        d("HB to MAV %s", node.getId());
         send(HB_PACKET, node);
       }
     }
@@ -124,27 +131,20 @@ public class MAVLinkCommunications extends Thread implements Debuggable {
       if (packet == null) {
         continue;
       }
-      // d("MSG: %d, %d, %d, %d", packet.len, packet.sysid, packet.compid, packet.msgid);
+      d("PKT: %d, %d, %d, %d", packet.len, packet.sysid, packet.compid, packet.msgid);
+      MAVLinkMessage msg = packet.unpack();
       MAVLinkNode node = nodes.get(packet.sysid);
-      if (packet.msgid == msg_heartbeat.MAVLINK_MSG_ID_HEARTBEAT) {
-        msg_heartbeat msg = new msg_heartbeat();
-        try {
-          msg.unpack(packet.payload);
-          d("HB from MAV %d", packet.sysid);
-          if (msg.type == MAV_TYPE.MAV_TYPE_FIXED_WING) {
+      if (msg.msgid == msg_heartbeat.MAVLINK_MSG_ID_HEARTBEAT) {
+          msg_heartbeat hbMsg = (msg_heartbeat) msg;
+          d("HB from node %d (%s)", hbMsg.sysid, hbMsg.type);
+          if (hbMsg.type == MAV_TYPE.MAV_TYPE_FIXED_WING) {
             if (node == null) {
-              node =  new MAVLinkNode(packet.sysid, udpPacket.getSocketAddress());
-              nodes.put(packet.sysid, node);
-              d("New MAV %d", packet.sysid);
+              node =  new MAVLinkNode(hbMsg.sysid, udpPacket.getSocketAddress());
+              nodes.put(hbMsg.sysid, node);
+              d("New MAV %d", hbMsg.sysid);
             }
-            
-            node.onHeartbeat(msg);
+            node.onHeartbeat(hbMsg);
           }
-        }
-        catch (RuntimeException e) {
-          d("Error decoding heartbeat message");
-          e.printStackTrace(System.err);
-        }
       } else if (node != null) {
         node.handleIncomingPacket(packet);
       }
@@ -157,7 +157,6 @@ public class MAVLinkCommunications extends Thread implements Debuggable {
 
   private void send(byte[] data, MAVLinkNode node) {
     try {
-      d("HB to MAV %s", node.getId());
       udpSocket.send(new DatagramPacket(data, 0, data.length, node.getAddress()));
     } catch (IOException e) {
       d("Error sending data to node: %s [ %s ]", node.getId(), node.getAddress());
