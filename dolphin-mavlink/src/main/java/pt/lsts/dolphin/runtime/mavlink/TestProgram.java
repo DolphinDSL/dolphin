98795cfa8f80e349c67801860ebf2f2060194b2d
Eduardo Marques <edrdo@dcc.fc.up.pt>
2019-10-01 15:54:37 +0100

    Mavlink - small test program

--- dolphin-mavlink/src/main/java/pt/lsts/dolphin/runtime/mavlink/TestProgram.java
+++ dolphin-mavlink/src/main/java/pt/lsts/dolphin/runtime/mavlink/TestProgram.java
@@ -1,9 +1,27 @@
 package pt.lsts.dolphin.runtime.mavlink;
 
+import java.io.IOException;
+
+import pt.lsts.dolphin.util.Debug;
+
 public class TestProgram {
 
-  public TestProgram() {
-    // TODO Auto-generated constructor stub
-  }
+  public static void main(String[] args) throws IOException {
+    MAVLinkCommunications comm = MAVLinkCommunications.getInstance();
+    Debug.enable(System.out, false);
+    comm.start();
 
+    while(true) {
+      for (MAVLinkNode n : comm.getNodes()) {
+        n.getDownloadProtocol().start();
+      }
+      try { 
+        Thread.sleep(10000);
+      } 
+      catch (InterruptedException e) {
+        
+      }
+    }
+
+  }
 }
