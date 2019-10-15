5ccd2e1f36381ba92838c0ea38d8fc1091bacab1
Eduardo Marques <edrdo@dcc.fc.up.pt>
2019-10-15 19:43:43 +0100

    Fixed groovy version in scripts

--- scripts/dolphin-mavlink.sh
+++ scripts/dolphin-mavlink.sh
@@ -1,1 +1,1 @@
-java -Djava.net.preferIPv4Stack=true -cp ~/.m2/repository/org/codehaus/groovy/groovy-all/2.4.10/groovy-all-2.4.10.jar:dolphin-core/target/dolphin-core-0.1-SNAPSHOT.jar:dolphin-mavlink/target/dolphin-mavlink-0.1-SNAPSHOT.jar pt.lsts.dolphin.runtime.mavlink.Main $*
+java -Djava.net.preferIPv4Stack=true -cp ~/.m2/repository/org/codehaus/groovy/groovy-all/2.4.11/groovy-all-2.4.11.jar:dolphin-core/target/dolphin-core-0.1-SNAPSHOT.jar:dolphin-mavlink/target/dolphin-mavlink-0.1-SNAPSHOT.jar pt.lsts.dolphin.runtime.mavlink.Main $*
